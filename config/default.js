cd31e4e0b1a93376e4627afff1ea8648aa33909a
Alka <ghproject587@gmail.com>
2023-08-29 02:08:30 +0530

    final code
    hostel app deployment

---
+++ config/default.js
@@ -0,0 +1,13 @@
+module.exports={
+
+    // MONGODB_URL: 'mongodb://127.0.0.1:27017/ras',
+    MONGODB_URL: 'mongodb+srv://ghproject587:UMVo3kKv4QML58Wu@gh-project-db.pqthfgq.mongodb.net/?retryWrites=true&w=majority',
+    SECRET_KEY: '#ALKA$$##ADITI##$$SHIVANI$$$$HRIshita###',
+    GMAIL: 'anytime8271@gmail.com',
+    PASSWORD: 'Alka@7667'
+}
+
+// UMVo3kKv4QML58Wu
+// ghproject587
+// mongodb+srv://ghproject587:<password>@gh-project-db.pqthfgq.mongodb.net/?retryWrites=true&w=majority
+// mongoimport --uri mongodb+srv://tudor_15:UMVo3kKv4QML58Wu@cluster0.acapz.mongodb.net/Hostelproject --collection test.prevstudents --type CSV --file FRESHstudata.csv
